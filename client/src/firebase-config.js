c8dfd3af57993b833943659f636d9851efed3242
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-05-20 19:25:09 +0900

    feat: firebase init

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "@firebase/firestore";
-import dotenv from 'dotenv'
-dotenv.config()
+import { getFirestore } from "@firebase/firestore";
+import dotenv from "dotenv";
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optiona l
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyCpfM2w3l-ZrL2uUR2YcD8XuhQRdy2MH8k",
+  authDomain: "a-1c067.firebaseapp.com",
+  projectId: "a-1c067",
+  storageBucket: "a-1c067.appspot.com",
+  messagingSenderId: "1040195901987",
+  appId: "1:1040195901987:web:c86d5812a3be9c2ab62f96",
+  measurementId: "G-0XJ32B51X8",
 };
 
 // Initialize Firebase
